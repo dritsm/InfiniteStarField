db3f1e2db24ee3f8b123ca88b482989eadcee452
Kody Buss <kody.buss@gmail.com>
2016-01-27 22:40:19 -0600

    Finished moving starfield. Starship Player moves with controller.
    -Still needs rotation along x axis when rolling moving left or right.
    -Still needs boundaries for the screen
    -Still needs gravity on joystick when let go.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed = 0.1f;
+
+	private float x, y;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.GetAxis ("Horizontal");
+		y = Input.GetAxis ("Vertical");
+
+		transform.position = transform.position +
+			new Vector3 (x * speed, y * speed, 0);
+	}
+}
---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour 
+{
+	private float currentOffset = 0;
+	public float speed = .1f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		currentOffset += Time.deltaTime * speed;  
+		if (currentOffset > 1.0f) 
+		{
+			currentOffset -= 1.0f;
+		}   
+
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, currentOffset); 
+	}
+}
