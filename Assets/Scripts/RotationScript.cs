1ee519e266700f642f4a01f641add2cdb3ae8dc3
Kody Buss <kody.buss@gmail.com>
2016-01-28 22:15:06 -0600

    HW 2 finished with these added features: -forward faced tilting based on the the direction pressed on the horizontal axis -smooth transition back to default positon -adjustments to screen size and boundaries

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	private float x;
+	private float _rotationZ = 0f;
+
+	public float angleSensitivityHor = 9.0f;
+	//public float minimumHor = -45.0f;//special case variables
+	//public float maximumHor = 45.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		Rigidbody body = GetComponent<Rigidbody> ();
+		if (body != null)
+			body.freezeRotation = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		x = Input.GetAxis ("Horizontal");
+ 
+		_rotationZ = x * angleSensitivityHor;// use -= if I want to maintain angle each frame
+		transform.Rotate (0f, 0f , _rotationZ);// rotate object
+
+		//_rotationZ = Mathf.Clamp (_rotationZ, minimumHor, maximumHor);//Used if want to clamp angle by 45
+
+		float rotationY = transform.localEulerAngles.y;//scalable y reset positon based off current position
+
+		transform.localEulerAngles = new Vector3 (0f, rotationY , _rotationZ * -1); //resets position
+
+	}
+}
